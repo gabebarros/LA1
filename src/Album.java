9c8a586169daa58c28b7ca1705b5570992c4c99c
gabebarros <gabe.barros@icloud.com>
2025-02-20 19:09:44 -0700

    Implemented file parsing logic

---
+++ src/Album.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Album {
+	
+	private String title;
+	private String artist;
+	private String genre;
+	private int year;
+	private ArrayList<String> tracklist;
+	
+	public Album(String title, String artist, String genre, int year, ArrayList<String> tracklist) {
+		this.title = title;
+		this.artist = artist;
+		this.genre = genre;
+		this.year = year;
+		this.tracklist = tracklist;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+}
--- src/MusicStore.java
+++ src/MusicStore.java
@@ -1,7 +1,84 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MusicStore {
+	private ArrayList<Album> albumList;
+	
+	private static ArrayList<Album> constructAlbumList() {
+		File albums = new File("albums/albums.txt");  // hold albums.txt
+		BufferedReader reader = null;
+		
+		// set reader to albums.txt
+		try {
+			reader = new BufferedReader(new FileReader(albums));
+		} catch (FileNotFoundException e) {
+			System.out.println("albums.txt could not be found");
+            System.exit(-1);
+		}
+		
+		String line = null;  // content of one line of the albums file
+		ArrayList<String> albumFileNameList = new ArrayList<String>();
+		
+		// read each line of albums.txt and construct a list of the album file names
+        try {
+			while((line = reader.readLine()) != null) {
+			    String albumName = line.split(",")[0];  // extract album title
+			    String artist = line.split(",")[1];  // extract artist
+			    
+			    String albumFileName = albumName + "_" + artist + ".txt";
+			    
+			    albumFileNameList.add(albumFileName);
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading from albums.txt");
+            System.exit(-1);
+		}
+        
+        ArrayList<Album> albumList = new ArrayList<Album>();  // list of album objects
+        
+        // construct each album object and add it to albumList
+        for (String albumFileName : albumFileNameList) {
+        	File curAlbumFile = new File("albums/" + albumFileName);
+        	
+        	try {
+				reader = new BufferedReader(new FileReader(curAlbumFile));
+				
+				String firstLine = reader.readLine();
+				String albumName = firstLine.split(",")[0];  
+			    String artist = firstLine.split(",")[1]; 
+			    String genre = firstLine.split(",")[2];  
+			    int year = Integer.parseInt(firstLine.split(",")[3]); 
+			    
+			    ArrayList<String> tracklist = new ArrayList<String>();
+				
+				while((line = reader.readLine()) != null) {
+				    tracklist.add(line);
+				}
+				
+				Album curAlbum = new Album(albumName, artist, genre, year, tracklist);
+				albumList.add(curAlbum);
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("File " + albumFileName + " could not be found");
+				System.exit(-1);
+			} catch (IOException e) {
+                System.out.println("Error reading file: " + albumFileName);
+                System.exit(-1);
+            }
+        }
+        
+        return albumList;
+	}
 	
 	public static void main(String[]args) {
-		
+		ArrayList<Album> albumList = constructAlbumList();
+				
+		for (Album a : albumList) {
+			System.out.println(a.getTitle());
+		}
 	}
 }
